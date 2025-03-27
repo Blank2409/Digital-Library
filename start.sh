deb2cd2a75c9f809cf96fd37d5c1c5ff7069ba6e
Blank2409 <eshan.kesarwani@gmail.com>
2025-03-27 22:21:11 +0530

    Add startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Absolute path to Java
-/usr/local/bin/java -jar target/*.jar
+# Use the exact Java path we know exists after installation
+/usr/lib/jvm/java-17-openjdk-amd64/bin/java -jar target/*.jar
