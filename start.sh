4ec9aa2de443efbb9966dc4f1a9bcc2d410b5b8c
Blank2409 <eshan.kesarwani@gmail.com>
2025-03-27 22:13:24 +0530

    Add foolproof Java startup

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Find Java in standard locations
-if [ -f "/usr/bin/java" ]; then
-  /usr/bin/java -jar target/*.jar
-elif [ -f "/usr/lib/jvm/java-17-openjdk-amd64/bin/java" ]; then
-  /usr/lib/jvm/java-17-openjdk-amd64/bin/java -jar target/*.jar
-else
-  echo "ERROR: Java not found in expected locations"
-  exit 1
-fi
+# Absolute path to Java
+/usr/local/bin/java -jar target/*.jar
