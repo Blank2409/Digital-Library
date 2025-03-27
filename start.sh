a83c0df318bc140f83255a2d84b9a501286867c4
Blank2409 <eshan.kesarwani@gmail.com>
2025-03-27 22:09:00 +0530

    Add startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# Find Java automatically
-JAVA_PATH=$(update-alternatives --list java | grep "java-17" | head -1)
-exec "$JAVA_PATH" -jar target/*.jar
+# Find Java in standard locations
+if [ -f "/usr/bin/java" ]; then
+  /usr/bin/java -jar target/*.jar
+elif [ -f "/usr/lib/jvm/java-17-openjdk-amd64/bin/java" ]; then
+  /usr/lib/jvm/java-17-openjdk-amd64/bin/java -jar target/*.jar
+else
+  echo "ERROR: Java not found in expected locations"
+  exit 1
+fi
