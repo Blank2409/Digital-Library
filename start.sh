780417389e3ac6af4173a07a70a5ef8892b0a27c
Blank2409 <eshan.kesarwani@gmail.com>
2025-03-27 22:25:17 +0530

    Add startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use the exact Java path we know exists after installation
-/usr/lib/jvm/java-17-openjdk-amd64/bin/java -jar target/*.jar
+/usr/lib/jvm/java-19-openjdk-amd64/bin/java -jar target/*.jar
